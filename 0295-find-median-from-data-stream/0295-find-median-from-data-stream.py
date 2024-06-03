14333a8318934b7a3ee4fde77af9bb83f45d9e03
Sonu kumar <99523590+SonukumarCSE@users.noreply.github.com>
2024-06-03 17:33:03 +0530

    Time: 372 ms (57.5%), Space: 38.1 MB (81.94%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,20 +1,34 @@
+import heapq
+
 class MedianFinder:
-
     def __init__(self):
-        self.arr = []
-
+        self.left_max_heap = []  # Max heap (invert values to use with min heap)
+        self.right_min_heap = []  # Min heap
 
     def addNum(self, num: int) -> None:
-        bisect.insort(self.arr, num)
-    def findMedian(self) -> float:
-        n = len(self.arr)
-        if n % 2 == 0:
-            return (self.arr[n//2] + self.arr[(n//2)-1]) / 2
+        if not self.left_max_heap or num < -self.left_max_heap[0]:
+            heapq.heappush(self.left_max_heap, -num)  # Push negative value to simulate max heap
         else:
-            return self.arr[n//2]
+            heapq.heappush(self.right_min_heap, num)
 
+        # Always maintain left_max_heap size one greater than right_min_heap size
+        # or both heaps' sizes equal
+        if len(self.left_max_heap) > len(self.right_min_heap) + 1:
+            heapq.heappush(self.right_min_heap, -heapq.heappop(self.left_max_heap))
+        elif len(self.left_max_heap) < len(self.right_min_heap):
+            heapq.heappush(self.left_max_heap, -heapq.heappop(self.right_min_heap))
 
-# Your MedianFinder object will be instantiated and called as such:
-# obj = MedianFinder()
-# obj.addNum(num)
-# param_2 = obj.findMedian()
+    def findMedian(self) -> float:
+        if len(self.left_max_heap) == len(self.right_min_heap):
+            # Even number of elements
+            return (-self.left_max_heap[0] + self.right_min_heap[0]) / 2.0
+        # Odd number of elements
+        return -self.left_max_heap[0]
+
+# Example usage:
+# medianFinder = MedianFinder()
+# medianFinder.addNum(1)
+# medianFinder.addNum(2)
+# print(medianFinder.findMedian())  # Output: 1.5
+# medianFinder.addNum(3)
+# print(medianFinder.findMedian())  # Output: 2
