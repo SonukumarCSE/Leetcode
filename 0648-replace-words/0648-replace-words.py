2c382a0fd34bf2161bd6a425c27cb7fe5febd2c9
Sonu kumar <99523590+SonukumarCSE@users.noreply.github.com>
2024-06-07 09:00:56 +0530

    Time: 7792 ms (5.03%), Space: 24.4 MB (99.76%) - LeetHub

---
+++ 0648-replace-words/0648-replace-words.py
@@ -0,0 +1,19 @@
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        word = sentence.split(" ")
+        dictionary.sort()
+        for i in range(len(dictionary)):
+            
+            for j in range(len(word)):
+                k = 0
+                hai = True
+                while k < len(dictionary[i]) and k < len(word[j]):
+                    if dictionary[i][k] != word[j][k]:
+                        hai = False
+                        break
+                    k += 1
+                if hai:
+                    if len(word[j]) > len(dictionary[i]):
+                        word[j] = dictionary[i]
+    
+        return " ".join(word)
