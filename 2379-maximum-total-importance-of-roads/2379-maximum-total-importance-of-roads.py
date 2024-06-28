7abe74dae8c7c2c70138de5db8d82f88952955ab
Sonu kumar <99523590+SonukumarCSE@users.noreply.github.com>
2024-06-28 07:53:27 +0530

    Time: 1213 ms (75.41%), Space: 42.1 MB (60.66%) - LeetHub

--- 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.py
+++ 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.py
@@ -5,7 +5,7 @@ class Solution:
             count[u] += 1
             count[v] += 1
         count.sort()
-        print(count)
+        # print(count)
         ans = 0
         for i in range(0,n):
             ans += (i+1) * count[i]
