29d6c8b173f44d618297dd96d3feb0da7e0eac3b
Sonu kumar <99523590+SonukumarCSE@users.noreply.github.com>
2024-08-08 15:46:18 +0530

    Time: 32 ms (78.59%), Space: 16.4 MB (94.14%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,17 +1,8 @@
 class Solution:
-    def __init__(self):
-        self.memo = {}
-    def solve(self,row,col,m,n):
-        if (row,col) in self.memo:
-            return self.memo[(row,col)]
-        if row == 0 and col == 0:
-            return 1
-        if row < 0 or row >= m or col < 0 or col >= n:
-            return 0
-        up = self.solve(row-1,col,m,n)
-        left = self.solve(row,col-1,m,n)
-        self.memo[(row,col)] = up + left
-        return self.memo[(row,col)]
     def uniquePaths(self, m: int, n: int) -> int:
-        
-        return self.solve(m-1,n-1,m,n)
+        N = n + m - 2
+        r = m - 1
+        res = 1
+        for i in range(1, r + 1):
+            res = res * (N - r + i) / i
+        return int(res)
