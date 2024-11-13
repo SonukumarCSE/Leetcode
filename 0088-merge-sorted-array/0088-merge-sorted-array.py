85c097b4694aaac6af095c5da358a319964ea6e4
Sonu kumar <99523590+SonukumarCSE@users.noreply.github.com>
2024-11-13 22:41:52 +0530

    Time: 0 ms (100%), Space: 16.6 MB (47.02%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,20 +3,22 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        first = 0
-        second = 0
-        for i in range(n):
+        for x in range(n):
             nums1.pop()
-        while first < len(nums1) and second < len(nums2):
-            print(nums1)
-            if nums1[first] <= nums2[second]:
-                first += 1
+        start = 0
+        end = 0
+        while start < m and end < n:
+            if nums1[start] < nums2[end]:
+                start += 1
             else:
-                nums1.insert(first,nums2[second])
-                first += 1
-                second += 1
-        # first += 1
-        while second < n:
-            nums1.insert(first,nums2[second])
-            first += 1
-            second += 1
+                print(start)
+                nums1.insert(start,nums2[end])
+                start += 1
+                m += 1
+                end += 1
+
+        while end < n:
+            nums1.append(nums2[end])
+            end += 1
+
+            
